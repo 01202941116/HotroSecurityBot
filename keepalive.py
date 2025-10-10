0df844c834f745308889756ee29b770cd5fb23f0
HotroSecurityBot <long223330@gmail.com>
2025-10-10 16:14:19 +0700

    Delete keepalive.py

--- keepalive.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app=Flask(__name__)
-@app.get('/')
-def i(): return {'ok':True}
-
-def run():
- import os; app.run('0.0.0.0', int(os.getenv('PORT','10000')), debug=False, use_reloader=False)
