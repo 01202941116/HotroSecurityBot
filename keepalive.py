46ecb949780ea278b6b55fba88032fb986c36152
HotroSecurityBot <long223330@gmail.com>
2025-10-10 18:35:35 +0700

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -1,13 +1,12 @@
-
+# keepalive.py
 from flask import Flask
-import os
+from threading import Thread
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return {"ok": True, "service": "HotroSecurityBot"}
+    return "ok", 200
 
 def run():
-    port = int(os.environ.get("PORT", "10000"))
-    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=10000, debug=False)
