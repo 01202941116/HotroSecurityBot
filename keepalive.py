4f3d743109623cf771454b1ac4df0a84d7728c79
HotroSecurityBot <long223330@gmail.com>
2025-10-10 22:58:44 +0700

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -1,14 +1,12 @@
-# keepalive.py
 import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def index():
     return "ok", 200
 
 def run():
-    # Render đặt PORT qua env, không hard-code cổng
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))  # Render cấp PORT
     app.run(host="0.0.0.0", port=port, threaded=True)
