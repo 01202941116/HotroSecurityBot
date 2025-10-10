59a5679f3b86cf1c7dec45a373a9562f688a7ec9
HotroSecurityBot <long223330@gmail.com>
2025-10-10 20:35:14 +0700

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -1,12 +1,24 @@
-# keepalive.py
 from flask import Flask
 from threading import Thread
+import requests, time
 
 app = Flask(__name__)
 
-@app.get("/")
-def index():
-    return "ok", 200
+@app.route('/')
+def home():
+    return "OK", 200
 
 def run():
-    app.run(host="0.0.0.0", port=10000, debug=False)
+    app.run(host='0.0.0.0', port=10000)
+
+def ping():
+    while True:
+        try:
+            requests.get("https://hotrosecuritybot.onrender.com")
+        except:
+            pass
+        time.sleep(300)  # 5 phút ping 1 lần
+
+def start_keepalive():
+    Thread(target=run).start()
+    Thread(target=ping).start()
