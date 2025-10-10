0f37897c9d00b144d718771d7b46f404f0c888a3
HotroSecurityBot <long223330@gmail.com>
2025-10-10 15:24:54 +0700

    Delete pro directory

--- pro/scheduler.py
+++
@@ -1,8 +0,0 @@
-
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from .license_manager import check_and_downgrade_expired
-
-def attach_scheduler(application):
-    sched = AsyncIOScheduler(timezone="UTC")
-    sched.add_job(check_and_downgrade_expired, "interval", hours=1, id="license_expire_check")
-    sched.start()
