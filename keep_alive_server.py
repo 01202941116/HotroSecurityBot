fd79d2e5a9df171a69fc4e9b760265e01b5b2de6
HotroSecurityBot <long223330@gmail.com>
2025-10-11 13:33:11 +0700

    Update keep_alive_server.py

--- keep_alive_server.py
+++ keep_alive_server.py
@@ -1,16 +1,19 @@
-# keepalive.py
+# keep_alive_server.py
 from flask import Flask
 from threading import Thread
-from keepalive import keep_alive
-app = Flask('')
+
+# Tạo Flask app
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bot is alive!"
 
+# Chạy webserver để Render giữ bot online
 def run():
     app.run(host='0.0.0.0', port=8080)
 
+# Hàm gọi song song, giúp bot không bị sleep
 def keep_alive():
     t = Thread(target=run)
     t.start()
